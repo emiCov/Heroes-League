e3c9b499b9f5e9cf36a19542b9e2739e75234033
Emi Covreag <millycov@yahoo.com>
2022-02-13 18:24:10 +0200

    Implemented Pyromancer's abilities

--- src/com/emi/Pyromancer.java
+++ src/com/emi/Pyromancer.java
@@ -5,44 +5,116 @@ public class Pyromancer extends Hero {
         super(name, hitPoints, positionX, positionY);
     }
 
+    // Fireblast
+
     @Override
     public void firstAbility(Knight knightHero, Terrain terrain) {
+        float damage = 350f + 50 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 1.2f;                              // apply the race modifier
+
+        knightHero.takeDamage(Math.round(damage));
     }
 
     @Override
     public void firstAbility(Pyromancer pyromancerHero, Terrain terrain) {
+        float damage = 350f + 50 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 0.9f;                              // apply the race modifier
+
+        pyromancerHero.takeDamage(Math.round(damage));
     }
 
     @Override
     public void firstAbility(Wizard wizardHero, Terrain terrain) {
+        float damage = 350f + 50 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 1.05f;                             // apply the race modifier
+
+        wizardHero.takeDamage(Math.round(damage));
     }
 
     @Override
     public void firstAbility(Rogue rogueHero, Terrain terrain) {
+        float damage = 350f + 50 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 0.8f;                              // apply the race modifier
+
+        rogueHero.takeDamage(Math.round(damage));
     }
+
+    // Ignite
 
     @Override
     public void secondAbility(Knight knightHero, Terrain terrain) {
+        float damage = 150f + 20 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 1.2f;                              // apply the race modifier
+
+        knightHero.takeDamage(Math.round(damage));
+        knightHero.ignite(
+                Math.round((50 + 30 * this.getLevel()) * 1.2f),
+                2);               // ignite the opponent
     }
 
     @Override
     public void secondAbility(Pyromancer pyromancerHero, Terrain terrain) {
+        float damage = 150f + 20 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 0.9f;                              // apply the race modifier
+
+        pyromancerHero.takeDamage(Math.round(damage));
+        pyromancerHero.ignite(
+                Math.round((50 + 30 * this.getLevel()) * 0.9f),
+                2);               // ignite the opponent
     }
 
     @Override
     public void secondAbility(Wizard wizardHero, Terrain terrain) {
+        float damage = 150f + 20 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 1.05f;                             // apply the race modifier
+
+        wizardHero.takeDamage(Math.round(damage));
+        wizardHero.ignite(
+                Math.round((50 + 30 * this.getLevel()) * 1.05f),
+                2);               // ignite the opponent
     }
 
     @Override
     public void secondAbility(Rogue rogueHero, Terrain terrain) {
+        float damage = 150f + 20 * this.getLevel(); // apply the level modifier
 
+        if (terrain == Terrain.VOLCANIC)
+            damage *= 1.25f;                         // apply the terrain modifier
+
+        damage *= 0.8f;                              // apply the race modifier
+
+        rogueHero.takeDamage(Math.round(damage));
+        rogueHero.ignite(
+                Math.round((50 + 30 * this.getLevel()) * 0.8f),
+                2);               // ignite the opponent
     }
 
     @Override
